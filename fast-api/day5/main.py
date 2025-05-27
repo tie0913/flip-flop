74c7379681a84e50d185ed5615e024023c8475ad
Wang <wang9431@saskpolytech.ca>
2025-05-27 16:41:51 -0600

    day5 is done

--- fast-api/day5/main.py
+++ fast-api/day5/main.py
@@ -45,3 +45,54 @@ async def get_service_name(name:str, provider:Service = Depends(get_service)):
 
     return {"msg": msg}
 
+
+
+# async middleware.
+# this looks like filters or interceptors in Spring
+# we may use it to implement basic functions, such as user validation, authentication , authorization and so forth.
+# and we may return the request before executing acutual method.
+#
+# if we want to execute next method in the chain we may invoke call_next method
+#      response = call_next(request)
+
+# Startlette provides these features which can be async
+# so we need to import
+from starlette.requests import Request
+
+# 1. add time comsumed by the method in the response header
+
+import time
+@server.middleware("http")
+async def add_time_consumed(req:Request, call_next):
+    start = time.monotonic()
+    resp = await call_next(req)
+    duration = time.monotonic() - start
+    resp.headers["X-Process-Time"] = str(duration)
+    return resp
+
+
+# 2. adding headers (CORS/Meta)
+@server.middleware("http")
+async def add_custom_header(req:Request, call_next):
+    resp = await call_next(req)
+    resp.headers["X-Custom-Header"] = "FastAPI-Rocks"
+    return resp
+
+
+# 3.coroutine-safe logging
+import logging
+@server.middleware("http")
+async def log(req:Request, call_next):
+    logging.info(f"Request: {req.method} {req.url}")
+    resp = await call_next(req)
+    logging.info(f"Response status:{resp.status_code}")
+    return resp
+    
+
+# now launch the uvicorn and visit a http method
+# when we define a middleware it will apply to all the http method
+# if we want a middleware method only apply on specific method, we need to do custom logic in that middleware method.
+
+# the fast api will invoke all the middleware methods in the reverse order of definition.
+# if you want to specify orders of the middleware methods. you may use 'server.addMiddleware' method
+
